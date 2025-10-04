131e1f058933cc27773888ebc890bfffb3b51952
Diksha <diksha252006@gmail.com>
2025-10-04 13:06:24 +0530

    created folders

---
+++ frontend/opphora/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <h1 className="text-3xl font-bold text-center mt-10">
+        Welcome to Opphora
+      </h1>
+    </div>
+  );
+}
+
+export default App;
