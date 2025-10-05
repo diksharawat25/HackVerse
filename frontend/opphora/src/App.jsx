c5fb99772aeba1704e4ff563afe3ee140e073722
gungun95 <uk609500@gmail.com>
2025-10-05 16:24:27 +0530

    update

--- frontend/opphora/src/App.jsx
+++ frontend/opphora/src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return <Home />;
 }
-
+function App(){
+  return <Login />
+}
 export default App;
