74b3775ead4a9f0de4ef2696e38ca10183b4f5b3
Basudhachauhan46 <basudhachauhan685@gmail.com>
2025-10-05 08:20:18 +0530

    updates

--- frontend/opphora/src/pages/Home.jsx
+++ frontend/opphora/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import HeroSection from "../components/heroSection";
+import HeroSection from "../components/HeroSection";
 import EventBanner from "../components/EventBanner";
 import Footer from "../components/Footer";
 
@@ -15,4 +15,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Home;
